41cd713b8db849ea39a9a92c94c196621c21fcda
Joshua M Freedholm <freedholmj@gmail.com>
2020-04-22 19:02:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function happyHolidays(string) {
   return "Happy holidays!"
 }
 function happyHolidaysTo(string) {
-  console.log(`Happy holidays, ${}")
+  console.log(`Happy holidays, ${name}!")
 }
